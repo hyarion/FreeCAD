8a656be086e5a14b6e8b32d281e4a7bec10b4e98
Gregg Jaskiewicz <gryzman@gmail.com>
2026-03-19 14:31:06 +0000

    Add Section Analysis feature for interactive cross-section visualization.

--- src/Mod/Part/App/AppPart.cpp
+++ src/Mod/Part/App/AppPart.cpp
@@ -87,6 +87,7 @@
 #include "FeaturePartImportStep.h"
 #include "FeaturePartPolygon.h"
 #include "FeaturePartSection.h"
+#include "FeatureSectionAnalysis.h"
 #include "FeaturePartSpline.h"
 #include "FeatureProjectOnSurface.h"
 #include "FeatureRevolution.h"
@@ -458,6 +459,7 @@ PyMOD_INIT_FUNC(Part)
     Part::Fuse                  ::init();
     Part::MultiFuse             ::init();
     Part::Section               ::init();
+    Part::SectionAnalysis       ::init();
     Part::FilletBase            ::init();
     Part::Fillet                ::init();
     Part::Chamfer               ::init();
